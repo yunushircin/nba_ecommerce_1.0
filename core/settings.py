370e4d7ea929ad48cf365ffcc86ffa4d95f0f2e2
yunushircin <yunushircin@gmail.com>
2023-02-13 17:34:47 +0300

    gunicorn

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-sqf4^_je72-2)wntl6=+w2t#m*s=!kokoqk=)(h#ys2mc()xvk
 DEBUG = True
 # DEBUG = getenv("IS_DEVELOPMENT", True)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['nba-ecommerce.herokuapp.com']
 
 # 'env-nbaecommerce.eba-eeeshp6r.us-west-2.elasticbeanstalk.com'
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
